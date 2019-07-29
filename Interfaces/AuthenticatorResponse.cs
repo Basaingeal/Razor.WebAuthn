88ca8df32fc681e32e0b5780cb914aaa7d463e60
Michael J. Currie <mcurrie@agwsinc.com>
2019-07-29 08:51:55 -0500

    updates

--- Interfaces/AuthenticatorResponse.cs
+++ Interfaces/AuthenticatorResponse.cs
@@ -3,7 +3,7 @@
     public abstract class AuthenticatorResponse
     {
 #pragma warning disable CA1819 // Properties should not return arrays
-        public byte[] ClientDataJSON { get; set; }
+        public byte[] ClientDataJSON { get; internal set; }
 #pragma warning restore CA1819 // Properties should not return arrays
     }
 }
